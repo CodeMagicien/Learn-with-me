58c4984458835b434eace4bf5b18b258b74acbc2
Ishrak Abdullah <127308776+CodeMagicien@users.noreply.github.com>
2025-01-15 09:27:40 -0800

    Add files via upload

---
+++ 01_hellocode.c
@@ -0,0 +1,25 @@
+//Boiler plate code (must for every program in every lang) :-
+//
+// #include<stdio.h>
+// 
+// int main(){
+//     printf(" Hello World! ")
+//     return 0;
+//  }
+
+#include<stdio.h>  //one kind of Preprocessor Directive
+                        // called as "Header file" also
+               // Necessary function before starting the program
+//tells compiler to include the standard input-output h. file
+
+#include<stdlib.h>
+#include<time.h> // A library for time
+#include<math.h>
+
+int main(){  // all c program start from main() function
+                    // executing start from line by line 
+    
+    printf("Hello World \n"); // execution start from here 
+   
+    return 0;
+}
